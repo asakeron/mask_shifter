2e8d41bc0643e1b551e306d262e58b24a2c4e5b9
André Silva <andre.silva@iea.sp.gov.local>
2022-07-06 16:32:22 -0300

    Rewrite implementation to be null safe

--- lib/mask_shifter.dart
+++ lib/mask_shifter.dart
@@ -1,16 +1,13 @@
 library mask_shifter;
 
-import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 
 class MaskedTextInputFormatterShifter extends TextInputFormatter {
   String maskONE;
   String maskTWO;
 
-  MaskedTextInputFormatterShifter({this.maskONE, this.maskTWO}) {
-    assert(maskONE != null);
-    assert(maskTWO != null);
-  }
+  MaskedTextInputFormatterShifter(
+      {required this.maskONE, required this.maskTWO});
 
   @override
   TextEditingValue formatEditUpdate(
@@ -19,31 +16,35 @@ class MaskedTextInputFormatterShifter extends TextInputFormatter {
       if (newValue.text.length > oldValue.text.length) {
         if (newValue.text.length > maskTWO.length) return oldValue;
 
-        if (newValue.text.length <= maskONE.length && (maskONE[newValue.text.length - 1] != "X") && newValue.text.length != oldValue.text.length) {
+        if (newValue.text.length <= maskONE.length &&
+            (maskONE[newValue.text.length - 1] != "X") &&
+            newValue.text.length != oldValue.text.length) {
           if (maskONE[newValue.text.length - 1] != "X") {
             return TextEditingValue(
-              text: '${oldValue.text}' + maskONE[newValue.text.length - 1] + '${newValue.text.substring(newValue.text.length - 1)}',
+              text: '${oldValue.text}' +
+                  maskONE[newValue.text.length - 1] +
+                  '${newValue.text.substring(newValue.text.length - 1)}',
               selection: TextSelection.collapsed(
                 offset: newValue.selection.end + 1,
               ),
             );
           }
         } else if (newValue.text.length > maskONE.length) {
-          String TWO = "";
+          String two = "";
 
           if (oldValue.text.length == maskONE.length) {
             newValue.text.runes.forEach((int rune) {
               var character = new String.fromCharCode(rune);
               if (character != "." && character != "-" && character != "/") {
-                TWO = TWO + character;
-                if (maskTWO[TWO.length] != "X") {
-                  TWO = '$TWO' + maskTWO[TWO.length];
+                two = two + character;
+                if (maskTWO[two.length] != "X") {
+                  two = '$two' + maskTWO[two.length];
                 }
               }
             });
 
             return TextEditingValue(
-              text: '$TWO',
+              text: '$two',
               selection: TextSelection.collapsed(
                 offset: newValue.selection.end + 1,
               ),
@@ -52,24 +53,18 @@ class MaskedTextInputFormatterShifter extends TextInputFormatter {
         }
       } else if (oldValue.text.length > newValue.text.length) {
         if (oldValue.text.length == (maskONE.length + 1)) {
-          String ONE = "";
+          String one = "";
           newValue.text.runes.forEach((int rune) {
             var character = new String.fromCharCode(rune);
             if (character != "." && character != "-" && character != "/") {
-              ONE = ONE + character;
-              try{
-                if (maskONE[ONE.length] != "X") {
-                  ONE = '$ONE' + maskONE[ONE.length];
-                }
+              one = one + character;
+              if (maskONE[one.length] != "X") {
+                one = '$one' + maskONE[one.length];
               }
-              catch(Exception){
-
-              }
-
             }
           });
           return TextEditingValue(
-            text: '$ONE',
+            text: '$one',
             selection: TextSelection.collapsed(
               offset: newValue.selection.end,
             ),
--- test/mask_shifter_test.dart
+++ test/mask_shifter_test.dart
@@ -1,7 +1,1 @@
-import 'package:test/test.dart';
-import 'package:mask_shifter/mask_shifter.dart';
-
-void main() {
-
-
-}
+void main() {}
